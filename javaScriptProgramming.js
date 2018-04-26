e93075156faccb56612cb6581f0247558669d7b4
Vikas Bose <nodejquery11@gmail.com>
2018-04-26 09:55:17 +0530

    update extension and add few more function

--- javaScriptProgramming.js
+++ javaScriptProgramming.js
@@ -95,7 +95,50 @@ var fontColors = function(str, colorValue) {
 var a = "hello world";
 fontColors(a, "yellow");
 
+/* Return the string value according to index number */
+let arrCharIndex = function(str, cb) {      [create callback method]
+  return cb(str, i);
+};
+var i = 2;
+var b = arrCharIndex(str, function(d, i){   [define the callback function]
+    let a = str.split("");
+    return a[i];
+});
+console.log(b);
 
+/* Return the ascii code based on the char */
+const asciiCode = {
+  "31": "",      "32": " ",     "33": "!",     "34": "\"",    "35": "#",    
+"36": "$",     "37": "%",     "38": "&",     "39": "'",     "40": "(",    
+"41": ")",     "42": "*",     "43": "+",     "44": ",",     "45": "-",    
+"46": ".",     "47": "/",     "48": "0",     "49": "1",     "50": "2",    
+"51": "3",     "52": "4",     "53": "5",     "54": "6",     "55": "7",    
+"56": "8",     "57": "9",     "58": ":",     "59": ";",     "60": "<",    
+"61": "=",     "62": ">",     "63": "?",     "64": "@",     "65": "A",    
+"66": "B",     "67": "C",     "68": "D",     "69": "E",     "70": "F",    
+"71": "G",     "72": "H",     "73": "I",     "74": "J",     "75": "K",    
+"76": "L",     "77": "M",     "78": "N",     "79": "O",     "80": "P",    
+"81": "Q",     "82": "R",     "83": "S",     "84": "T",     "85": "U",    
+"86": "V",     "87": "W",     "88": "X",     "89": "Y",     "90": "Z",    
+"91": "[",     "92": "\\",    "93": "]",     "94": "^",     "95": "_",    
+"96": "`",     "97": "a",     "98": "b",     "99": "c",     "100": "d",    
+"101": "e",    "102": "f",    "103": "g",    "104": "h",    "105": "i",    
+"106": "j",    "107": "k",    "108": "l",    "109": "m",    "110": "n",    
+"111": "o",    "112": "p",    "113": "q",    "114": "r",    "115": "s",    
+"116": "t",    "117": "u",    "118": "v",    "119": "w",    "120": "x",    
+"121": "y",    "122": "z",    "123": "{",    "124": "|",    "125": "}",    
+"126": "~",    "127": ""
+};
+
+let findAsciiCode = function(str, index) {
+   var charCode = str.split("")[index];
+   return Object.keys(asciiCode).filter(function(item, i) {        [it returns an array so zeroth place contain the value]
+      return charCode === asciiCode[item] && asciiCode[item] !== undefined;
+   })[0];
+};
+
+var c = findAsciiCode(str, 1);
+console.log(c); // return the 101 the value of e`
 
 
 
